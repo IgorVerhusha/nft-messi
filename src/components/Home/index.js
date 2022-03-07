c9e2e4709d8afaf957aa10d564d99821daf03d13
IgorVerhusha <viarkhusha93@gmail.com>
2022-03-07 15:06:08 +0300

    fixed layout

--- src/components/Home/index.js
+++ src/components/Home/index.js
@@ -60,7 +60,7 @@ const Home = (props) => {
             style.left = "-2%";
         } else {
             style.bottom = "6%";
-            style.right = "16%";
+            style.right = "17%";
             style.left = "48%";
         }
 
